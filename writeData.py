38c56358d66783d15d4d4c17309a81fbd1d3e447
AlexHorn <ahorn@shoppertrak.com>
2016-11-13 01:12:58 -0600

    elevation added to export via google api

--- writeData.py
+++ writeData.py
@@ -24,6 +24,10 @@ client = MongoClient(config.get("data","host"), int(config.get("data","port")))
 db = client[config.get("data","db")]
 data = db[config.get("data","collection")]
 
+# defaults
+
+lat = "42.3601"
+lon = "-71.0589"
 
 # get from db
 #post_id = data.insert_one(r.json()).inserted_id
@@ -63,4 +67,54 @@ def writeCSVfromDB():
             outFileWriter.writerow(row)
             #outFileWriter.flush()
 
-writeCSVfromDB()
+### comment in to run
+#writeCSVfromDB()
+
+def writeElevationToMongo():
+    for i,d in enumerate(data.find()):
+        print i
+        print d['_id']
+        h = getElevation(lat,long)
+        d.update({'elevation':h})
+        data.save(d)
+        #update d w/ {'elevation':h}
+        #d.update
+        print "updated record"
+        #if i > 5:
+        #    break
+        
+# mycollection.update({'_id':mongo_id}, {"$set": post}, upsert=False)
+
+# get elevation
+
+    # let's get elevation from lat lon
+    # then use it to update items in the existing collection
+    # perhpas multiprocess 
+
+def getElevation(lat, lon):
+    key = "97529b523e71d8d51576ce9bbb89a0ce"
+    baseURL = "https://maps.googleapis.com/maps/api/elevation/json?locations="
+    excludeString = "exclude=currently,minutely,hourly,flags"
+    lat = "42.3601"
+    lon = "-71.0589"
+    gkey = "AIzaSyBcq1h9jN7YsphDV_QQVlirwsoGy3d6EGk"
+    unixtime = "1420498800"
+    #getURL = baseURL+key+"/"+str(lat)+","+str(lat)+","+str(time)+"?"+excludeString
+    
+    getURL = "https://maps.googleapis.com/maps/api/elevation/json?locations=%s,%s&key=%s" % (lat, lon, gkey)
+    
+    print getURL
+
+    r = requests.get(getURL)
+    
+    
+    elevation = r.json()['results'][0]['elevation']
+    print elevation 
+    return elevation 
+    # write to db
+    #post_id = data.insert_one(r.json()).inserted_id
+    #print("written to db")
+
+#getElevation(lat, lon)
+
+writeElevationToMongo()
